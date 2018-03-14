6d2b12b537d47efcbbb65dfa4649c055d29d90c2
jiben22 <benji.legallo22@live.fr>
2018-03-14 09:01:09 +0100

    Add views

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,12 +1,26 @@
 <?php
 class Home extends CI_Controller {
     
-    public function index () {
+    public function index () {      
       //Define view to load for content
       $data['content'] = 'home';
       //Give name file of view
       $this->load->vars($data);
       $this->load->view('template');
     }
+  
+  public function createPost() {
+    $this->form_validation->set_rules('post', 'required');
+    if(!$this->form_validation->run() === FALSE) {
+      $post = $this->input->post('post');
+      $data['post'] = $post;
+    }
+    
+    //Define view to load for content
+    $data['content'] = 'home';
+    //Give name file of view
+    $this->load->vars($data);
+    $this->load->view('template');
+  }
 }
 ?>
---
+++ application/views/template/aside.php
@@ -0,0 +1,112 @@
+<!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="<?php echo base_url()."assets/"; ?>dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>Benjamin LE GALLO</p>
+          <a href="<?php echo base_url()."assets/"; ?>#"><i class="fa fa-circle text-success"></i>Online</a>
+        </div>
+      </div>
+      <!-- search form
+      <form action="#" method="get" class="sidebar-form">
+        <div class="input-group">
+          <input type="text" name="q" class="form-control" placeholder="Search...">
+          <span class="input-group-btn">
+                <button type="submit" name="search" id="search-btn" class="btn btn-flat">
+                  <i class="fa fa-search"></i>
+                </button>
+              </span>
+        </div>
+      </form>
+      <!-- /.search form -->
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">Menu de navigation</li>
+        <li class="active treeview menu-open">
+          <a href="<?php echo base_url()."assets/"; ?>#">
+            <i class=" glyphicon glyphicon-th-list"></i> <span>Liste</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="<?php echo base_url()."assets/"; ?>index.html"><i class="fa fa-circle-o"></i>Utilisateurs</a></li>
+            <li class="active"><a href="<?php echo base_url()."assets/"; ?>index2.html"><i class="fa fa-circle-o"></i>Amis</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="<?php echo base_url()."assets/"; ?>pages/mailbox/mailbox.html">
+            <i class="fa fa-envelope"></i> <span>Mailbox</span>
+            <span class="pull-right-container">
+              <small class="label pull-right bg-yellow">12</small>
+              <small class="label pull-right bg-green">16</small>
+              <small class="label pull-right bg-red">5</small>
+            </span>
+          </a>
+        </li>
+        <li class="treeview">
+          <a href="<?php echo base_url()."assets/"; ?>#">
+            <i class="fa fa-folder"></i> <span>Examples</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="<?php echo base_url()."assets/"; ?>pages/examples/invoice.html"><i class="fa fa-circle-o"></i> Invoice</a></li>
+            <li><a href="<?php echo base_url()."assets/"; ?>pages/examples/profile.html"><i class="fa fa-circle-o"></i> Profile</a></li>
+            <li><a href="<?php echo base_url()."assets/"; ?>pages/examples/login.html"><i class="fa fa-circle-o"></i> Login</a></li>
+            <li><a href="<?php echo base_url()."assets/"; ?>pages/examples/register.html"><i class="fa fa-circle-o"></i> Register</a></li>
+            <li><a href="<?php echo base_url()."assets/"; ?>pages/examples/lockscreen.html"><i class="fa fa-circle-o"></i> Lockscreen</a></li>
+            <li><a href="<?php echo base_url()."assets/"; ?>pages/examples/404.html"><i class="fa fa-circle-o"></i> 404 Error</a></li>
+            <li><a href="<?php echo base_url()."assets/"; ?>pages/examples/500.html"><i class="fa fa-circle-o"></i> 500 Error</a></li>
+            <li><a href="<?php echo base_url()."assets/"; ?>pages/examples/blank.html"><i class="fa fa-circle-o"></i> Blank Page</a></li>
+            <li><a href="<?php echo base_url()."assets/"; ?>pages/examples/pace.html"><i class="fa fa-circle-o"></i> Pace Page</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="<?php echo base_url()."assets/"; ?>#">
+            <i class="fa fa-share"></i> <span>Multilevel</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="<?php echo base_url()."assets/"; ?>#"><i class="fa fa-circle-o"></i> Level One</a></li>
+            <li class="treeview">
+              <a href="<?php echo base_url()."assets/"; ?>#"><i class="fa fa-circle-o"></i> Level One
+                <span class="pull-right-container">
+                  <i class="fa fa-angle-left pull-right"></i>
+                </span>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="<?php echo base_url()."assets/"; ?>#"><i class="fa fa-circle-o"></i> Level Two</a></li>
+                <li class="treeview">
+                  <a href="<?php echo base_url()."assets/"; ?>#"><i class="fa fa-circle-o"></i> Level Two
+                    <span class="pull-right-container">
+                      <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                  </a>
+                  <ul class="treeview-menu">
+                    <li><a href="<?php echo base_url()."assets/"; ?>#"><i class="fa fa-circle-o"></i> Level Three</a></li>
+                    <li><a href="<?php echo base_url()."assets/"; ?>#"><i class="fa fa-circle-o"></i> Level Three</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <li><a href="<?php echo base_url()."assets/"; ?>#"><i class="fa fa-circle-o"></i> Level One</a></li>
+          </ul>
+        </li>
+        <li><a href="<?php echo base_url()."assets/"; ?>https://adminlte.io/docs"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
+        <li class="header">LABELS</li>
+        <li><a href="<?php echo base_url()."assets/"; ?>#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
+        <li><a href="<?php echo base_url()."assets/"; ?>#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
+        <li><a href="<?php echo base_url()."assets/"; ?>#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li>
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
