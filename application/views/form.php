d01aa0dd19bb343be30ee39e6fbe9aa3c2175c73
jiben22 <benji.legallo22@live.fr>
2018-03-14 08:14:34 +0100

    Clean older files and add header

--- application/views/form.php
+++
@@ -1,9 +0,0 @@
-<h2>Créer une tâche</h2>
-
-<?php echo validation_errors(); ?>
-
-<?php echo form_open('todo/create') ?>
-    <label for="title">Enoncé de la tâche</label>
-    <input type="input" name="title"/><br/>
-    <input type="submit" name="submit" value="Créer une tâche"/>
-</form>
