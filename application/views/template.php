364971e5dc280aeafcd9b8eb085d27c803937314
jiben22 <benji.legallo22@live.fr>
2018-03-18 10:39:42 +0100

    Add favicon

--- application/views/template.php
+++ application/views/template.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>VisageLivre</title>
+  <link rel="icon" type="image/gif" href="<?php echo base_url()."assets/img/icon"?>.gif" />
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
